cf5a3702112dd81db195e5fb4f937b1dbec09fba
Maurik Holtrop <maurik@physics.unh.edu>
2020-11-24 13:55:27 -0500

    Initial commit of cmake package export scheme. Still needs configure_package_config_file implementation.

---
+++ cmake/EvioToolConfig.cmake
@@ -0,0 +1,9 @@
+#
+# By hand version of the Config.cmake file.
+#
+include(CMakeFindDependencyMacro)
+
+find_dependency(evio CONFIG REQUIRED)
+find_dependency(ROOT REQUIRED COMPONENTS Core)
+
+include("${CMAKE_CURRENT_LIST_DIR}/EvioToolTargets.cmake")
